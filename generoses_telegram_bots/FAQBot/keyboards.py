e82ffbdf4773d959d28da63ff1c33c996958b1f4
seregatipich <seregatipich@outlook.com>
2023-12-08 20:39:00 +0100

    added fix reminder

--- generoses_telegram_bots/FAQBot/keyboards.py
+++ generoses_telegram_bots/FAQBot/keyboards.py
@@ -1,11 +1,11 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-def create_inline_kb_main(main_menu_buttons: list) -> InlineKeyboardBuilder:
+def create_inline_kb_main(buttons_names: list) -> InlineKeyboardBuilder:
 
     builder = InlineKeyboardBuilder()
-    for i in range(0, len(main_menu_buttons)):
-        builder.button(text=f"{main_menu_buttons[i]}", callback_data=f"btn{i}")
-        builder.adjust(2, 2)
+    for i in range(0, len(buttons_names)):
+        builder.button(text=f"{buttons_names[i]}", callback_data=f"btn{i}")
+        builder.adjust(2, 2) # ПОФИКСИТЬ - СДЕЛАТЬ АРГУМЕНТОМ ФУНКЦИИ
         
     return builder.as_markup()
 
