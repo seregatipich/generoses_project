ed629e5332400cbb6ef3947c9dde218f6fa813fb
TheDodge25 <demux.dozhdikov@gmail.com>
2023-12-09 19:58:50 +0300

    Moved handlers in a proper folder

---
+++ generoses_telegram_bots/FAQBot/handlers/welcome.py
@@ -0,0 +1,14 @@
+from aiogram import Router
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+
+from keyboards import inline_kb
+
+rt = Router()
+
+@rt.message(CommandStart())
+async def welcome(message: Message) -> None:
+    await message.answer(
+        f"Привет! Это бот, который поможет вам разобраться в проблеме с GENEROSES. Чтобы начать, выберете пункт меню, который вам нужен",
+        reply_markup=inline_kb(main_menu_buttons, 2)
+    )
