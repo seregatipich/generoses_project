fb2cd316c9fb274abb9420cf030925af1aeae7bd
TheDodge25 <demux.dozhdikov@gmail.com>
2023-12-10 19:55:42 +0300

    Updated all handlers

--- generoses_telegram_bots/FAQBot/handlers/sub_categories.py
+++ generoses_telegram_bots/FAQBot/handlers/sub_categories.py
@@ -7,6 +7,7 @@ from keyboards import inline_kb
 rt = Router()
 
 
+# Начало хэндлеров для категории "Пожертвования"
 @rt.callback_query(lambda c: c.data == 'Управление')
 async def handle_button_one_sub0(callback_query: types.CallbackQuery) -> None:
     await callback_query.message.answer('\n\n'.join(format_subsection_qa_pairs_json('Пожертвования')['Управление пожертвованиями']))
@@ -23,45 +24,44 @@ async def handle_button_one_sub1(callback_query: types.CallbackQuery) -> None:
 async def handle_button_one_sub2(callback_query: types.CallbackQuery) -> None:
     await callback_query.message.answer('\n\n'.join(format_subsection_qa_pairs_json('Пожертвования')['Технические вопросы и проблемы']))
     await callback_query.answer()
+# Конец хэндлеров для категории "Пожертвования"
 
 
-@rt.callback_query(lambda c: c.data == 'btn1_sub0')
+# Начало хэндлеров для категории "Как это работает?"
+@rt.callback_query(lambda c: c.data == 'Общий')
 async def handle_button_one_sub0(callback_query: types.CallbackQuery) -> None:
-    await callback_query.message.answer(text="BTN1_0")
+    await callback_query.message.answer('\n\n'.join(format_subsection_qa_pairs_json('Как это работает?')['Общий Процесс и Механизмы']))
     await callback_query.answer()
 
 
-@rt.callback_query(lambda c: c.data == 'btn1_sub1')
+@rt.callback_query(lambda c: c.data == 'Руководства')
 async def handle_button_one_sub1(callback_query: types.CallbackQuery) -> None:
-    await callback_query.message.answer(text="BTN1_1")
+    await callback_query.message.answer('\n\n'.join(format_subsection_qa_pairs_json('Как это работает?')['Руководства и Инструкции']))
     await callback_query.answer()
 
 
-@rt.callback_query(lambda c: c.data == 'btn1_sub2')
+@rt.callback_query(lambda c: c.data == 'Политика')
 async def handle_button_one_sub2(callback_query: types.CallbackQuery) -> None:
-    await callback_query.message.answer(text="BTN1_2")
+    await callback_query.message.answer('\n\n'.join(format_subsection_qa_pairs_json('Как это работает?')['Политика и Принципы']))
     await callback_query.answer()
+# Конец хэндлеров для категории "Как это работает?"
 
 
-@rt.callback_query(lambda c: c.data == 'btn2')
-async def handle_button_one(callback_query: types.CallbackQuery) -> None:
-    await callback_query.message.answer(text="BTN2")
-    await callback_query.answer()
-
-
-@rt.callback_query(lambda c: c.data == 'btn2_sub0')
+# Начало хэндлеров для категории "Участие и Волонтерство"
+@rt.callback_query(lambda c: c.data == 'Способы')
 async def handle_button_one_sub0(callback_query: types.CallbackQuery) -> None:
-    await callback_query.message.answer(text="BTN2_0")
+    await callback_query.message.answer('\n\n'.join(format_subsection_qa_pairs_json('Участие и Волонтерство')['Способы Участия']))
     await callback_query.answer()
 
 
-@rt.callback_query(lambda c: c.data == 'btn2_sub1')
+@rt.callback_query(lambda c: c.data == 'Преимущества')
 async def handle_button_one_sub1(callback_query: types.CallbackQuery) -> None:
-    await callback_query.message.answer(text="BTN2_1")
+    await callback_query.message.answer('\n\n'.join(format_subsection_qa_pairs_json('Участие и Волонтерство')['Преимущества и Возможности']))
     await callback_query.answer()
 
 
-@rt.callback_query(lambda c: c.data == 'btn2_sub2')
+@rt.callback_query(lambda c: c.data == 'Поддержка')
 async def handle_button_one_sub2(callback_query: types.CallbackQuery) -> None:
-    await callback_query.message.answer(text="BTN2_2")
+    await callback_query.message.answer('\n\n'.join(format_subsection_qa_pairs_json('Участие и Волонтерство')['Поддержка и Ресурсы']))
     await callback_query.answer()
+# Конец хэндлеров для категории "Участие и Волонтерство"
